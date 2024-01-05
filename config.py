3a9773fd5b3fc2af463fc9af136f428447540a0b
Apiquet <anthony.piquet2@gmail.com>
2024-01-05 14:28:23 +0100

    feat: update config to new 14 features

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ from torchmetrics.classification import MulticlassAccuracy
 
 from data.datasets.datasets_utils import get_train_val_splits
 from data.datasets.mini_france.mini_france_dataset import MiniFranceDataset
-from data.datasets.mini_france.mini_france_utils import REMAP_LABELS
+from data.datasets.mini_france.mini_france_utils import REMAP_LABELS, REMAP_LABELS_TO_RGB
 from data.preprocessing.labels_preprocessing import remap_labels
 from models.unet import UNet
 from viewers.viewers import MiniFranceSamplesViewer
@@ -23,20 +23,20 @@ class Config:
     def __init__(self, db_path: Path, gee_project_name: Optional[str] = None) -> None:
         """Init all training variables."""
         # Training vars
-        self.batch_size = 8
-        self.epochs_count = 1
+        self.batch_size = 16
+        self.epochs_count = 100
         self.loss_func = CrossEntropyLoss(ignore_index=0)
 
         # Dataset variables
-        self.keep_n_elements = 80
-        self.train_ratio = 0.5
+        self.keep_n_elements = None
+        self.train_ratio = 0.8
         self.db_path = db_path
         self.features_preprocess = None
         self.labels_preprocess = [lambda x: remap_labels(x, remap_names=REMAP_LABELS)]
 
         # Model variables
-        self.number_of_classes = len(REMAP_LABELS)
-        self.model = UNet(in_channels=2, n_classes=self.number_of_classes, padding=True)
+        self.number_of_classes = len(REMAP_LABELS_TO_RGB)
+        self.model = UNet(in_channels=14, n_classes=self.number_of_classes, padding=True)
         self.model.to(device("cuda" if cuda.is_available() else "cpu"))
 
         # Optimizer variables
@@ -45,10 +45,15 @@ class Config:
 
         # Dataset
         dataset = MiniFranceDataset(
-            db_path=self.db_path, keep_n_elements=self.keep_n_elements, features_preprocess=self.features_preprocess, labels_preprocess=self.labels_preprocess, gee_project_name=gee_project_name
+            db_path=self.db_path,
+            train_ratio=self.train_ratio,
+            keep_n_elements=self.keep_n_elements,
+            features_preprocess=self.features_preprocess,
+            labels_preprocess=self.labels_preprocess,
+            gee_project_name=gee_project_name,
         )
         self.train_dl, self.val_dl, self.train_ds, self.val_ds = get_train_val_splits(dataset=dataset, batch_size=self.batch_size, train_ratio=self.train_ratio)
 
         # Metrics
         self.metrics: list[Metric] = [MulticlassAccuracy(self.number_of_classes)]
-        self.viewers: list[Callable] = [MiniFranceSamplesViewer(samples_count_to_visualize=15, dataset=self.val_ds, batch_size=self.batch_size)]
+        self.viewers: list[Callable] = [MiniFranceSamplesViewer(samples_count_to_visualize=30, dataset=self.val_ds, batch_size=self.batch_size, classes_to_rgb=REMAP_LABELS_TO_RGB)]
