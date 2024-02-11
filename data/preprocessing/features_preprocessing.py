deea2964ada923f5c1422eeac76d417f7342069d
Apiquet <anthony.piquet2@gmail.com>
2024-02-11 11:43:33 +0100

    feat: preprocessing now takes tensors

--- data/preprocessing/features_preprocessing.py
+++ data/preprocessing/features_preprocessing.py
@@ -1,19 +1,19 @@
 """Function to preprocess features."""
 
-import numpy as np
+from torch import Tensor
 
 
-def normalization_per_channel(features: np.ndarray, mean_values: np.ndarray, std_values: np.ndarray) -> np.ndarray:
+def normalization_per_channel(features: Tensor, mean_values: Tensor, std_values: Tensor) -> Tensor:
     """Normalize each feature channel: (feature-mean)/std.
 
     Args:
-        features (np.ndarray): tensor format [C,H,W]
-        mean_values (np.ndarray): C values
-        std_values (np.ndarray): C values
+        features (Tensor): tensor format [C,H,W]
+        mean_values (Tensor): C values
+        std_values (Tensor): C values
 
     Returns:
         Tensor: normalized tensor
     """
-    normalize_features = features - mean_values[None, None, :]
-    normalize_features /= std_values[None, None, :]
+    normalize_features = features - mean_values[:, None, None]
+    normalize_features /= std_values[:, None, None]
     return normalize_features
